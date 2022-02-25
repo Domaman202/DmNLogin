d7209b69467184b52b51d1e1226d80e8bf9af645
Domaman202 <vip.domaman@mail.ru>
2022-02-25 20:12:19 +0700

    xD

--- src/main/java/me/londiuh/login/listeners/OnPlayerMove.java
+++ src/main/java/me/londiuh/login/listeners/OnPlayerMove.java
@@ -2,6 +2,7 @@ package me.londiuh.login.listeners;
 
 import me.londiuh.login.LoginMod;
 import me.londiuh.login.PlayerLogin;
+import net.minecraft.entity.Entity;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
@@ -13,6 +14,10 @@ public class OnPlayerMove {
         boolean isLoggedIn = playerLogin.isLoggedIn();
         if (!isLoggedIn) {
             player.teleport(player.getX(), player.getY(), player.getZ()); // teleport to sync client position
+            if (player.hasVehicle()) {
+                Entity vehicle = player.getVehicle();
+                vehicle.teleport(player.getX(), vehicle.getY(), player.getZ());
+            }
         }
         return isLoggedIn;
     }
